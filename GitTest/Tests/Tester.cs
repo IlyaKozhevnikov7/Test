9cacdb9d0003a1be8d8e1690199ba07d6df0e9b5
IlyaKozhevnikov7 <skeletor1215@gmail.com>
2022-06-06 17:54:34 +0300

    Added test three

--- GitTest/Tests/Tester.cs
+++ GitTest/Tests/Tester.cs
@@ -6,5 +6,6 @@ namespace GitTest
         public void Test() { }
         public void NewTest() {}
         public bool Test2() { return true; }
+        public bool Test3() { return false; }
     }
 }
