bbccdd943d2650a5d24bc9632b5f34eacc1d0840
IlyaKozhevnikov7 <skeletor1215@gmail.com>
2022-06-06 17:48:56 +0300

    Added test two

--- GitTest/Tests/Tester.cs
+++ GitTest/Tests/Tester.cs
@@ -4,6 +4,7 @@ namespace GitTest
     public class Tester
     {
         public void Test() { }
-        public void NewTest2() {}
+        public void NewTest() {}
+        public bool Test2() { return true; }
     }
 }
